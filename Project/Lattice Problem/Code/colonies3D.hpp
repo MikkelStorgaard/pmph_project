3c35ac023e120505546effd475835328f9a073f1
Rasmus Skytte Eriksen <rasmusskytteeriksen@gmail.com>
2018-10-05 12:25:29 +0200

    Started to change from Armadillo to Arrays

--- Project/Lattice Problem/Code/colonies3D.hpp
+++ Project/Lattice Problem/Code/colonies3D.hpp
@@ -130,6 +130,11 @@ class Colonies3D {
     arma::Cube<double> I8_new;      // Infected bacteria
     arma::Cube<double> I9_new;      // Infected bacteria
 
+    double*** Arr_B; //  B[i][j][k]
+    double*** Arr_P;
+    double*** Arr_I0;
+    // ...
+
 
     // Nutrient matrix
     arma::cube   nutrient;
