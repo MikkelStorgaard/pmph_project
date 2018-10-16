37adc0f472581716facedd6c66bf3f52c1659a09
vfr988 <vfr988@a00331.science.domain>
2018-10-16 18:54:45 +0200

    Oops, forgot to have a main file

---
+++ Project/Lattice Problem/Code/cuda_version/example3D.cu
@@ -0,0 +1,51 @@
+#include "colonies3D.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+     // Default parameters
+    int nGrid 	 = 10;
+    int beta  	 = 100;
+    double delta = 1.0/10.0;
+    double eta   = 1e4;
+    double tau   = 0.5;
+
+    int k = 0;
+
+    for (int i = 0; i < argc; i++) {
+        if (i == 1) {
+            k = atoi(argv[i]);
+        }
+        if (i == 2) {
+            nGrid = atoi(argv[i]);
+        }
+    }
+
+    double B_0 = pow(10,4);
+    double P_0 = pow(10,5);
+    double T   = 5;
+
+    string pathName = "3D_Example";
+
+    // Load simulation module
+    Colonies3D s(B_0, P_0);
+
+    s.SetPath(pathName);
+
+    s.SetRngSeed(5);
+
+    s.SetGridSize(nGrid);
+    s.PhageBurstSize(beta);
+    s.PhageDecayRate(delta);
+    s.PhageAdsorptionRate(eta);
+    s.PhageLatencyTime(tau);
+    s.SetSamples(10);
+
+    // s.ExportAll();
+
+    // Start the simulation
+    s.Run_LoopDistributed_CPU(T);
+
+    return 0;
+}
