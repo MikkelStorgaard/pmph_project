81407a4f12fb16423f23fb8624d25d188dbfde04
Mikkel <mikkel@oldrup.org>
2018-10-24 00:05:47 +0200

    Adding a test/practice file for testing curand
    Tror jeg fik det til at virke i test-filen.

---
+++ Project/Lattice Problem/Code/cuda_version/curand_test.cu
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+#include <curand.h>
+#include <curand_kernel.h>
+#include <math.h>
+#include <assert.h>
+// #define ITER 10
+
+__global__ void setup_kernel(curandState *state){
+
+  int idx = threadIdx.x+blockDim.x*blockIdx.x;
+  curand_init(1234, idx, 0, &state[idx]);
+}
+
+__global__ void generate_kernel(curandState *my_curandstate, int iters, float *result, int *resultp){
+    int idx = threadIdx.x + blockDim.x*blockIdx.x;
+    result[idx] = curand_uniform(&my_curandstate[idx] + 1);
+    resultp[idx] = curand_poisson (&my_curandstate[idx], 10);
+}
+
+int main(){
+  int ITER = 10;
+    
+  curandState *d_state;
+  cudaMalloc(&d_state, ITER*sizeof(curandState));
+  
+  float *d_result;
+  int *d_resultp;
+  float *h_result = (float*) malloc(ITER*sizeof(float));
+  int *h_resultp = (int*) malloc(ITER*sizeof(int));
+  cudaMalloc(&d_result, ITER*sizeof(float));
+  cudaMalloc(&d_resultp, ITER*sizeof(int));
+  setup_kernel<<<1,ITER>>>(d_state);
+   
+  generate_kernel<<<1,ITER>>>(d_state, ITER, d_result, d_resultp);
+  cudaMemcpy(h_result, d_result, ITER*sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_resultp, d_resultp, ITER*sizeof(int), cudaMemcpyDeviceToHost);
+  for(int i = 0; i < ITER; i++){
+    printf("result : %f \n" , h_result[i]);
+    printf("resultp: %d \n" , h_resultp[i]);
+  }
+
+  return 0;
+}
