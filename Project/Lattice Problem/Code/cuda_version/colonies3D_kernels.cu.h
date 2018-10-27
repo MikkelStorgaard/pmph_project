2f694a0bde5d0034373c0c651b8f27acd03f0270
Rasmus Skytte Eriksen <rasmusskytteeriksen@gmail.com>
2018-10-27 23:19:27 +0200

    made the kernel version of ComputeDiffusion deterministic

--- Project/Lattice Problem/Code/cuda_version/colonies3D_kernels.cu.h
+++ Project/Lattice Problem/Code/cuda_version/colonies3D_kernels.cu.h
@@ -68,8 +68,8 @@ __device__ void ComputeDiffusion(curandState state, double n, double lambda, dou
 		*n_f = 0.0;
 		*n_b = 0.0;
 
-		// Trivial case
-		if (n < 1) return;
+		// DETERMINITIC CHANGE
+		// if (n < 1) return;
 
 		// Check if diffusion should occur
 		if ((lambda == 0) or (nGridXY == 1)) {
@@ -77,24 +77,25 @@ __device__ void ComputeDiffusion(curandState state, double n, double lambda, dou
 				return;
 		}
 
-		if (lambda*n < 5) {   // Compute all movement individually
+    // DETERMINITIC CHANGE
+		// if (lambda*n < 5) {   // Compute all movement individually
 
-				for (int l = 0; l < round(n); l++) {
+		// 		for (int l = 0; l < round(n); l++) {
 
-						double r = curand_uniform(&state);
+		// 				double r = curand_uniform(&state);
 
-						if       (r <    lambda)                     (*n_u)++;  // Up movement
-						else if ((r >=   lambda) and (r < 2*lambda)) (*n_d)++;  // Down movement
-						else if ((r >= 2*lambda) and (r < 3*lambda)) (*n_l)++;  // Left movement
-						else if ((r >= 3*lambda) and (r < 4*lambda)) (*n_r)++;  // Right movement
-						else if ((r >= 4*lambda) and (r < 5*lambda)) (*n_f)++;  // Forward movement
-						else if ((r >= 5*lambda) and (r < 6*lambda)) (*n_b)++;  // Backward movement
-						else                                         (*n_0)++;  // No movement
+		// 				if       (r <    lambda)                     (*n_u)++;  // Up movement
+		// 				else if ((r >=   lambda) and (r < 2*lambda)) (*n_d)++;  // Down movement
+		// 				else if ((r >= 2*lambda) and (r < 3*lambda)) (*n_l)++;  // Left movement
+		// 				else if ((r >= 3*lambda) and (r < 4*lambda)) (*n_r)++;  // Right movement
+		// 				else if ((r >= 4*lambda) and (r < 5*lambda)) (*n_f)++;  // Forward movement
+		// 				else if ((r >= 5*lambda) and (r < 6*lambda)) (*n_b)++;  // Backward movement
+		// 				else                                         (*n_0)++;  // No movement
 
-				}
+		// 		}
 
 
-		} else {
+		// } else {
 
 				// Compute the number of agents which move
 				double N = RandP(state, 3*lambda*n); // Factor of 3 comes from 3D
@@ -106,15 +107,15 @@ __device__ void ComputeDiffusion(curandState state, double n, double lambda, dou
 				*n_f = RandP(state, N/6);
 				*n_b = RandP(state, N/6);
 				*n_0 = n - (*n_u + *n_d + *n_l + *n_r + *n_f + *n_b);
-		}
+		// }
 
-		*n_u = round(*n_u);
-		*n_d = round(*n_d);
-		*n_l = round(*n_l);
-		*n_r = round(*n_r);
-		*n_f = round(*n_f);
-		*n_b = round(*n_b);
-		*n_0 = n - (*n_u + *n_d + *n_l + *n_r + *n_f + *n_b);
+		// *n_u = round(*n_u);
+		// *n_d = round(*n_d);
+		// *n_l = round(*n_l);
+		// *n_r = round(*n_r);
+		// *n_f = round(*n_f);
+		// *n_b = round(*n_b);
+		// *n_0 = n - (*n_u + *n_d + *n_l + *n_r + *n_f + *n_b);
 
 		// assert(*n_0 >= 0);
 		// assert(*n_u >= 0);
@@ -394,6 +395,7 @@ __global__ void NewInfectionsKernel(double* arr_Occ,
     // }
   }
 }
+
 __global__ void PhageDecay(double* arr_P, double p,
                            bool *warn_delta, curandState* rng_state,
                            bool* arr_IsActive){
